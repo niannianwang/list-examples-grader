308b0d252470ecdd7f41ff420b117b43d933b55e
niannianwang <niw002@ucsd.edu>
2024-05-28 10:01:53 -0700

    edit grader

--- TestListExamples.java
+++ TestListExamples.java
@@ -9,7 +9,23 @@ class IsMoon implements StringChecker {
   }
 }
 
+
 public class TestListExamples {
+  @Test
+  public void testFilter() {
+      List<String> inputList = Arrays.asList("moon", "sun", "star", "moon", "planet");
+      IsMoon isMoon = new IsMoon();
+
+      List<String> expectedResult = Arrays.asList("moon", "moon");
+
+      List<String> filteredList = ListExamples.filter(inputList, isMoon);
+
+      assertEquals(expectedResult.size(), filteredList.size());
+      for (String s : expectedResult) {
+          assertTrue(filteredList.contains(s));
+      }
+  }
+  
   @Test(timeout = 500)
   public void testMergeRightEnd() {
     List<String> left = Arrays.asList("a", "b", "c");
--- grade.sh
+++ grade.sh
@@ -1,3 +1,5 @@
+# Step 1 - clone the student's repository
+
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
@@ -8,6 +10,31 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+# Step 2 - check the student's submission contains the ListExamples.java file
+if [[ -f student-submission/ListExamples.java ]]
+then 
+    echo "file exists"
+else
+    echo "ListExamples.java does not exist"
+    echo "Grade: 0"
+    exit
+fi
+
+# Step 3 - put relevant file in grading-area folder
+cp student-submission/ListExamples.java TestListExamples.java grading-area
+cp -r lib grading-area
+
+# Step 4 - complie java file
+cd grading-area
+javac -cp $CPATH ListExamples.java TestListExamples.java 2> compile-error.txt
+# -cp followed by $CPATH tells java I am going to import $CPATH to help the compilation of later files
+# writes the error messages to 2> compile-error.txt
+
+echo "Exit code: $?"
+
+# Step 5 - grade report
+java TestListExamples.java 2> junit-output.txt
+# write JUnit test output to a text file
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
